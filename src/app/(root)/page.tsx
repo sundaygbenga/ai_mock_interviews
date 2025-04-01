a9628a6cfbff1482aa3eff96b91bd7f64572d863
sundaygbenga <sunflizasher@gmail.com>
2025-04-01 17:04:23 +0100

    show generated interviews on the homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,18 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
 import { dummyInterviews } from "@/constants";
+import {
+	getCurrentUser,
+	getInterviewsByUserId,
+} from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
 
-const page = () => {
+const Page = async () => {
+	const user = await getCurrentUser();
+	const userInterviews = await getInterviewsByUserId(user?.id!);
+
+	const hasPastInterviews = userInterviews ? userInterviews?.length > 0 : null;
 	return (
 		<>
 			<section className="card-cta ">
@@ -30,11 +38,13 @@ const page = () => {
 			<section className="flex flex-col gap-6 mt-8">
 				<h2>Your Interviews</h2>
 				<div className="interviews-section">
-					{dummyInterviews.map((interview) => (
-						<InterviewCard key={interview.id} {...interview} />
-					))}
-
-					{/* <p>You haven&apos;t taken any interviews yet</p> */}
+					{hasPastInterviews ? (
+						userInterviews?.map((interview) => (
+							<InterviewCard key={interview.id} {...interview} />
+						))
+					) : (
+						<p>You haven&apos;t taken any interviews yet</p>
+					)}
 				</div>
 			</section>
 			<section className="flex flex-col gap-6 mt-8">
@@ -50,4 +60,4 @@ const page = () => {
 	);
 };
 
-export default page;
+export default Page;
